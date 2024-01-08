1e8b27677cab3ae88efbdc7a7b46c37ac79e7af4
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-09 01:08:26 +0200

    Highes and Lowest

---
+++ 6kyu/Multiples of 3 or 5.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    if (n < 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    for (let i = 3; i < n; i++) {
+        if (i % 3 === 0 || i % 5 === 0) sum += i
+    }
+    return sum;
+}
